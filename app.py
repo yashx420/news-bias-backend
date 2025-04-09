d568b7be38ed762615c7b74b9f6699e522a64ca3
Yash <73090835+yashx420@users.noreply.github.com>
2025-04-09 18:14:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
+import os
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -37,5 +38,8 @@ def predict():
         "confidence": round(confidence, 3)
     })
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
